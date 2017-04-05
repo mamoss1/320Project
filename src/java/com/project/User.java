f8c6432674887af37cf840bd99adf735456661c8
mamoss1 <mamoss1@crimson.ua.edu>
2017-04-04 20:37:08 -0500

    add user.java (updated variables)

--- src/java/com/project/User.java
+++ src/java/com/project/User.java
@@ -7,8 +7,8 @@ import java.util.Date;
 public class User {
 
     private int user_id;
-    private String first_name;
-    private String last_name;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     private Boolean isManager;
@@ -21,21 +21,23 @@ public class User {
         this.user_id = user_id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
+
+    
 
     public String getEmail() {
         return email;
